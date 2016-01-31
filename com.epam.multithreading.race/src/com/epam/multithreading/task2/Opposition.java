dae1882a6c7081f173a02cb4a4b66ec2f766b541
Pavel <pashayehov93@gmail.com>
2016-01-31 22:54:53 +0300

    Modified task 2: replace checking of conditions and incrementatio/decrementation to synchronized block

--- com.epam.multithreading.race/src/com/epam/multithreading/task2/Opposition.java
+++ com.epam.multithreading.race/src/com/epam/multithreading/task2/Opposition.java
@@ -5,107 +5,113 @@ import java.util.Random;
 public class Opposition {
 
 	/**
-	 * Modified class counter. 
+	 * Modified class counter.
+	 * 
 	 * @author Pavel
-	 *
+	 * 
 	 */
-    public class Counter {
+	public class Counter {
 
-    	//Object which not allows to make count value < 0
-    	private final Object monitor = new Object();
-        private int count = 10;
+		// Object which not allows to make count value < 0
+		private final Object monitor = new Object();
+		private int count = 10;
 
-        public void increment() {
-            count++;
-            if(count == 1){
-            	  synchronized (monitor) {
-                      monitor.notifyAll();
-                  }
-            }
-        }
+		public void increment() {
+			
+			synchronized (monitor) {
+				count++;
+				if (count == 1) {
+					monitor.notifyAll();
+				}
+			}
 
-        public void decrement() {
-        	if(count==0){
-        		synchronized(monitor){
-        			try {
+		}
+
+		public void decrement() {
+
+			synchronized (monitor) {
+				if (count == 0) {
+					try {
 						monitor.wait();
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-        		}
-        	}
-            count--;
-        }
+				}
+				count--;
+			}
 
-        public int get() {
-            return count;
-        }
+		}
 
-    }
+		public int get() {
+			return count;
+		}
 
-    public class Wrestler implements Runnable {
+	}
 
-        private Counter counter;
+	public class Wrestler implements Runnable {
 
-        private boolean increment;
+		private Counter counter;
 
-        private Random rand;
+		private boolean increment;
 
-        public Wrestler(Counter counter, boolean increment) {
-            this.counter = counter;
-            this.increment = increment;
-            rand = new Random();
-        }
+		private Random rand;
 
-        @Override
-        public void run() {
-            while (true) {
-                if (increment) {
-                    counter.increment();
-                } else {
-                    counter.decrement();
-                }
+		public Wrestler(Counter counter, boolean increment) {
+			this.counter = counter;
+			this.increment = increment;
+			rand = new Random();
+		}
 
-                int x = counter.get();
-                if (x < 0) {
-                    t1.interrupt();
-                    t2.interrupt();
-                    throw new IllegalStateException("We have below zero!");
-                }
+		@Override
+		public void run() {
+			while (true) {
+				if (increment) {
+					counter.increment();
+				} else {
+					counter.decrement();
+				}
 
-                System.out.println("Wrestler" + Thread.currentThread().getName() + " " + x);
-                try {
-                    Thread.sleep(rand.nextInt(100));
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        }
+				int x = counter.get();
+				if (x < 0) {
+					t1.interrupt();
+					t2.interrupt();
+					throw new IllegalStateException("We have below zero!");
+				}
 
-    }
+				System.out.println("Wrestler"
+						+ Thread.currentThread().getName() + " " + x);
+				try {
+					Thread.sleep(rand.nextInt(100));
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
 
-    private Thread t1, t2;
+	}
 
-    public static void main(String[] args) {
-        new Opposition().start();
-    }
+	private Thread t1, t2;
 
-    private void start() {
-        Counter counter = new Counter();
-        t1 = new Thread(new Wrestler(counter, true));
-        t2 = new Thread(new Wrestler(counter, false));
-        t1.start();
-        t2.start();
-        try {
-            while (true) {
-                Thread.sleep(100);
-                if (!(t1.isAlive() && t2.isAlive())) {
-                    break;
-                }
-            }
-        } catch (InterruptedException e) {
-        }
-        System.out.println("Finished");
-    }
+	public static void main(String[] args) {
+		new Opposition().start();
+	}
+
+	private void start() {
+		Counter counter = new Counter();
+		t1 = new Thread(new Wrestler(counter, true));
+		t2 = new Thread(new Wrestler(counter, false));
+		t1.start();
+		t2.start();
+		try {
+			while (true) {
+				Thread.sleep(100);
+				if (!(t1.isAlive() && t2.isAlive())) {
+					break;
+				}
+			}
+		} catch (InterruptedException e) {
+		}
+		System.out.println("Finished");
+	}
 
 }
